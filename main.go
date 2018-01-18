0bc1b5a667e66889ebb7e06a9f8ba400ed0273aa
zunda <zundan@gmail.com>
2018-01-17 14:21:08 -1000

    Add doctype

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-const template = `
+const template = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
 <title>nanimona.in</title>
